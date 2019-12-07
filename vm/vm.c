ebdbb9c93c116d76a8302ff31fdfc272fd9984a8
jieyr3Rui <2930681195@qq.com>
2019-12-07 11:50:26 +0800

    read addresses

--- vm/vm.c
+++ vm/vm.c
@@ -1,32 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
+
+#define NUM_OF_ADDR 1000
+uint16_t *get_addresses(const char* filename, int num);
 
 int main(){ 
+    const char* filename = "addresses.txt";
+    uint16_t* addresses = get_addresses(filename, NUM_OF_ADDR);
+    // for(int ii = 0; ii < NUM_OF_ADDR; ii++){
+    //     printf("%d: %hd\n", ii+1, addresses[ii]);
+    // }
 
-    char szTest[1000] = {0};
+    return 0;
+}
 
-    int len = 0;
-
-    FILE *fp = fopen("addresses.txt", "r");
-
+/**
+ * 
+ * 
+ **/
+uint16_t *get_addresses(const char* fn, int num){
+    uint16_t *addr = (uint16_t*) malloc(sizeof(uint16_t)*num);
+    FILE *fp = fopen(fn, "r");
     if(NULL == fp){
         printf("failed to open dos.txt\n"); 
-        return 1;
+        return NULL;
     }
-
-    while(!feof(fp)){
-
-        memset(szTest,0, sizeof(szTest));
-
-        fgets(szTest,sizeof(szTest) - 1, fp); // 包含了换行符          
-
-        printf("%s",szTest);
+    for(int ii = 0; ii < num; ii++){
+        fscanf(fp, "%hu", &addr[ii]);
     }
-
     fclose(fp);
-
-    printf("\n");
-    return 0;
-
+    return addr;
 }
