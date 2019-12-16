b70eb8f19659d297724b0102fe42897769c3d594
jieyr3Rui <2930681195@qq.com>
2019-12-16 21:21:21 +0800

    chage mtest.c

--- mtest/mtest.c
+++ mtest/mtest.c
@@ -1,60 +1,26 @@
- /*
-2 * ch03-memaddr.c --- Show address of code, data and stack sections,
-3 * as well as BSS and dynamic memory.
-4 */
 #include <stdio.h>
-#include <malloc.h> /* for definition of ptrdiff_t on GLIBC */
+#include <malloc.h>
 #include <unistd.h>
-#include <alloca.h> /* for demonstration only */
+#include <alloca.h>
 
-extern void afunc(void); /* a function for showing stack growth */
-
-int bss_var; /* auto init to 0, should be in BSS */
-int data_var = 42; /* init to nonzero, should be data */
-int
-main(int argc, char **argv) /* arguments aren't used */
-{
-    char *p, *b, *nb;
-
-    printf("Text Locations:\n");
-    printf("\tAddress of main: %p\n", main);
-    printf("\tAddress of afunc: %p\n", afunc);
-
-    printf("Stack Locations:\n");
-    afunc();
-
-    p = (char *) alloca(32);
-    if (p != NULL) {
-        printf("\tStart of alloca()'ed array: %p\n", p);
-        printf("\tEnd of alloca()'ed array: %p\n", p + 31);
-    }
+extern void move(int ,char ,char ,char);
 
 
-    printf("Data Locations:\n");
-    printf("\tAddress of data_var: %p\n", & data_var);
-
-    printf("BSS Locations:\n");
-    printf("\tAddress of bss_var: %p\n", & bss_var);
-
-    b = sbrk((ptrdiff_t) 32); /* grow address space */
-    nb = sbrk((ptrdiff_t) 0);
-    printf("Heap Locations:\n");
-    printf("\tInitial end of heap: %p\n", b);
-    printf("\tNew end of heap: %p\n", nb);
-
-    b = sbrk((ptrdiff_t) -16); /* shrink it */
-    nb = sbrk((ptrdiff_t) 0);
-    printf("\tFinal end of heap: %p\n", nb);
+int main(){
+	int N;
+	
+	scanf("%d",&N);
+	move(N,'A','B','C');
+	return 0;
 }
-void
-afunc(void)
-{
-    static int level = 0; /* recursion level */
-    auto int stack_var; /* automatic variable, on stack */
-    if (++level == 3) /* avoid infinite recursion */
-        return;
 
-    printf("\tStack level %d: address of stack_var: %p\n",
-    level, & stack_var);
-    afunc(); /* recursive call */
+void move(int n,char a,char b,char c){
+	if(n == 1)printf("Move disk 1 from column %c to column %c.\n",a,c);
+	else{
+		move(n -1,a,c,b);
+
+		printf("Move disk %d from column %c to column %c.\n",n,a,c);
+		move(n-1,b,a,c);
+
+	}
 }
